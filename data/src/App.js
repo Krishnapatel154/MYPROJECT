c82bd3050cacb35129b314d7862f22d86463042c
Krishnapatel154 <157212290+Krishnapatel154@users.noreply.github.com>
2024-03-18 18:25:37 +0000

    data

---
+++ data/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import {usestate} from 'react'
+function App() {
+
+  const [value,setvalue]=usestate('goodmorning')
+  function msg()
+  {
+    setvalue('evryone')
+  }
+  return(
+    <div className='App'>
+      <h1>hello {value}</h1>
+      <button onClick={msg}>click me</button>
+    </div>
+  )
+}
+
+export default App;
